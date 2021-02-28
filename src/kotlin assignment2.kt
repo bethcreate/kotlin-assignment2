c5e1d5e101c544ce991fc70ab482f32242a8bbbf
bethcreate <kamaubetth@gmail.com>
2021-02-28 13:03:58 +0300

    second commit

---
+++ src/kotlin assignment2.kt
@@ -0,0 +1,30 @@
+fun main() {
+  name()
+    modulus()
+    var add=addition( 5,8,9,5,)
+    println(add)
+    interestingFact()
+    }
+fun name() {
+ var name="Hello Beth"
+ println(name)
+}
+fun modulus() {
+    var a=7
+    var b=2
+    var modulus=a%b
+    println(modulus)
+}
+fun addition(num1:Int, num2:Int,num3:Int,num4:Int):Int{
+    var add= num1 + num2 + num3 + num4
+    return add
+
+}
+fun interestingFact() {
+    var interestingFact=("quite")
+    println(interestingFact)
+
+}
+
+
+
